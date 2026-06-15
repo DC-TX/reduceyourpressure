da6287ac9ac527d32b921ec0f3d89483a39a37cd
DC-TX <Jb14514@163.com>
2026-06-15 19:29:18 +0800

    remove unused react entry

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.js';
-import './styles.css';
-
-const doc = globalThis['doc' + 'ument'];
-const rootNode = doc.querySelector('#root');
-createRoot(rootNode).render(React.createElement(App));
