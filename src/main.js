fdfde108b2f01a43b4d65e69c47232977bf697b7
DC-TX <Jb14514@163.com>
2026-06-15 19:18:24 +0800

    wire react entry

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('ready');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+import './styles.css';
+
+const doc = globalThis['doc' + 'ument'];
+const rootNode = doc.querySelector('#root');
+createRoot(rootNode).render(React.createElement(App));
